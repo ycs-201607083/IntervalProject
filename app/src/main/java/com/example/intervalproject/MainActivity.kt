d52963dcaec58127465b6a8e37f8cad447d36c1a
ycs-201607083 <80020080+ycs-201607083@users.noreply.github.com>
2023-04-14 12:35:50 +0900

    add: 메인 UI작성

---
+++ app/src/main/java/com/example/intervalproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.intervalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intervalproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+    }
+}
